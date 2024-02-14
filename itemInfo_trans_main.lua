a045b550faf886d9f851d165360b7c1566018447
raffles <53240858+holic-cat@users.noreply.github.com>
2024-02-14 22:56:08 +0800

    1

---
+++ itemInfo_trans_main.lua
@@ -0,0 +1,97 @@
+tbl = {
+	[70001] = {
+		unidentifiedDisplayName = "Mask",
+		unidentifiedResourceName = "마스크",
+		unidentifiedDescriptionName = { "Unknown Item, can be identified by using a ^6666CCMagnifier^000000." },
+		identifiedDisplayName = "Angel Wings",
+		identifiedResourceName = "angel_wings",
+		identifiedDescriptionName = {
+			"Wings from a lovely angeling.",
+			"Agi +5.",
+			"Attack Speed +7%.",
+			"Class:^6666CC Headgear^000000",
+			"Defense:^0000FF 0^000000",
+			"Position:^6666CC Lower^000000",
+			"Weight:^009900 10^000000",
+		},
+		slotCount = 1,
+		ClassNum = 5001
+	},
+	[70002] = {
+		unidentifiedDisplayName = "Mask",
+		unidentifiedResourceName = "마스크",
+		unidentifiedDescriptionName = { "Unknown Item, can be identified by using a ^6666CCMagnifier^000000." },
+		identifiedDisplayName = "Devil Wings",
+		identifiedResourceName = "devil_wings",
+		identifiedDescriptionName = {
+			"Wings from a Devil.",
+			"Str +3.",
+			"Vit +3.",
+			"Class:^6666CC Headgear^000000",
+			"Defense:^0000FF 0^000000",
+			"Position:^6666CC Lower^000000",
+			"Weight:^009900 10^000000",
+		},
+		slotCount = 1,
+		ClassNum = 5002
+	},
+	[70003] = {
+		unidentifiedDisplayName = "Mask",
+		unidentifiedResourceName = "마스크",
+		unidentifiedDescriptionName = { "Unknown Item, can be identified by using a ^6666CCMagnifier^000000." },
+		identifiedDisplayName = "Demon Wings",
+		identifiedResourceName = "demon_wings",
+		identifiedDescriptionName = {
+			"Wings from a Demon.",
+			"Dex +3.",
+			"Agi +3.",
+			"Class:^6666CC Headgear^000000",
+			"Defense:^0000FF 0^000000",
+			"Position:^6666CC Lower^000000",
+			"Weight:^009900 10^000000",
+		},
+		slotCount = 1,
+		ClassNum = 5003
+	},
+	[70004] = {
+		unidentifiedDisplayName = "Mask",
+		unidentifiedResourceName = "마스크",
+		unidentifiedDescriptionName = { "Unknown Item, can be identified by using a ^6666CCMagnifier^000000." },
+		identifiedDisplayName = "Torn Wings",
+		identifiedResourceName = "tornwings",
+		identifiedDescriptionName = {
+			"A set of lucky-charmed wings that solely exist only because of chance. Granted with the gift of luck, you can say it'll rub off on you.",
+			"Luk +10.",
+			"Perfect Dogde +3.",
+			"Critical Damage +5%.",
+			"Class:^6666CC Headgear^000000",
+			"Defense:^0000FF 0^000000",
+			"Position:^6666CC Lower^000000",
+			"Weight:^009900 10^000000",
+		},
+		slotCount = 1,
+		ClassNum = 5004
+	},
+}
+
+function main()
+	for ItemID, DESC in pairs(tbl) do
+		result, msg = AddItem(ItemID, DESC.unidentifiedDisplayName, DESC.unidentifiedResourceName, DESC.identifiedDisplayName, DESC.identifiedResourceName, DESC.slotCount, DESC.ClassNum)
+		if not result then
+			return false, msg
+		end
+		for k, v in pairs(DESC.unidentifiedDescriptionName) do
+			result, msg = AddItemUnidentifiedDesc(ItemID, v)
+			if not result then
+				return false, msg
+			end
+		end
+		for k, v in pairs(DESC.identifiedDescriptionName) do
+			result, msg = AddItemIdentifiedDesc(ItemID, v)
+			if not result then
+				return false, msg
+			end
+		end
+	end
+	return true, "good"
+end
